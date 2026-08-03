4f0ae929e2614d5ae1c737f1b0a043d5071c9e9a
Yash <kherayash0909@gmail.com>
2026-08-03 23:26:26 +0530

    summary cards active now

--- src/components/meals/NutritionSummary.jsx
+++ src/components/meals/NutritionSummary.jsx
@@ -1,32 +1,57 @@
 import SummaryCard from "./SummaryCard";
 
-export default function NutritionSummary(){
+export default function NutritionSummary({meals}){
+   
+   const totalCalories = meals.reduce((total, meal) => {
+    return total + meal.calories;
+}, 0);
+
+const totalProtein = meals.reduce((total, meal) => {
+    return total + meal.protein;
+}, 0);
+
+const totalCarbs = meals.reduce((total, meal) => {
+    return total + meal.carbs;
+}, 0);
+
+const totalFats = meals.reduce((total, meal) => {
+    return total + meal.fats;
+}, 0);
+console.log({
+    totalCalories,
+    totalProtein,
+    totalCarbs,
+    totalFats,
+});
+   
+   
     const summaryData = [
     {
         title: "Calories",
-        value: 2460,
+        value: totalCalories,
         unit: "kcal",
         color: "emerald",
     },
     {
         title: "Protein",
-        value: 185,
+        value: totalProtein,
         unit: "g",
         color: "orange",
     },
     {
         title: "Carbs",
-        value: 245,
+        value: totalCarbs,
         unit: "g",
         color: "blue",
     },
     {
         title: "Fat",
-        value: 72,
+        value: totalFats,
         unit: "g",
         color: "purple",
     },
 ];
+
     return(
      <div className="grid grid-cols-4 gap-6">
     {summaryData.map((item) => (
